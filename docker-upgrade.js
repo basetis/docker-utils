5e3a11ddd3b3cb51b4cda092932d44c58d188067
Alba Mendez <me@alba.sh>
2020-09-27 14:02:56 +0200

    initial commit; docker-upgrade tool

---
+++ docker-upgrade.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+//@ts-check
+
+const { argv } = require('yargs')
+const Docker = require('dockerode')
+process.on('unhandledRejection', up => { throw up })
+
+const [ containerId, imageId ] = argv._
+
+;(async function main() {
+    // Connect to docker
+    const engine = new Docker()
+    const container = engine.getContainer(containerId)
+
+    // Check image exists, pull otherwise
+    const image = engine.getImage(imageId)
+    try {
+        await image.inspect()
+    } catch (e) {
+        if (e.statusCode !== 404) throw e
+        console.log('Pulling image...')
+        await engine.pull(imageId)
+    }
+
+    // Check container exists and get its config
+    const containerInfo = await container.inspect()
+
+    console.log('Stopping & removing...')
+    await container.stop()
+    await container.remove()
+
+    console.log('Launching new container...')
+    const { Config, HostConfig, NetworkSettings, Name: name } = containerInfo
+    const newContainer = await engine.createContainer({
+        name, ...Config, HostConfig,
+        NetworkingConfig: NetworkSettings.Networks,
+        // overrides
+        Image: imageId,
+    })
+    await newContainer.start()
+
+    console.log('Done.')
+})()
